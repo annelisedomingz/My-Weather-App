5423e6236d604753cffc408f8871eebd44921e19
annelisedomingz <annelisedomingz@gmail.com>
2021-06-03 22:02:42 -0400

    Used Javascript to create forecast

--- src/app.js
+++ src/app.js
@@ -43,6 +43,37 @@ function searchCityInput(event) {
   search(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["sun", "mon", "tues", "web", "thu", "fri", "sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+
+          <div class="col-2 forecastPanel">
+            <div class="weather-forcast-date">${day}</div>
+
+            <img
+              src="http://openweathermap.org/img/wn/04d@2x.png"
+              alt="clear"
+              id="icon"
+              class="float-left"
+              width="70px"
+            />
+            <div class="weather-forcast-temperatures">
+              <span class="weather-forecast-temperature-max">90°</span>
+              <span class="weather-forecast-temperature-min">73°</span>
+            </div>
+          </div>
+      `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //temperature
 
 function showTemperature(response) {
@@ -128,3 +159,4 @@ let currentLocationButton = document.querySelector(".currentLocation");
 currentLocationButton.addEventListener("click", handleCurrentLoactionClick);
 
 search("New York");
+displayForecast();
