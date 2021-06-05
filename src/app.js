83949ee7516b1a595efb3613a1d29acd9585516c
annelisedomingz <annelisedomingz@gmail.com>
2021-06-04 23:56:02 -0400

    Made quick update to units in the api

--- src/app.js
+++ src/app.js
@@ -95,7 +95,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "8e4399f0d975e2878379c34ca4703cc5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
